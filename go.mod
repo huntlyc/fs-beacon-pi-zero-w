cd152c5e73918a0bf54160f2cfd50e7154410f20
Huntly Cameron <huntly.cameron@gmail.com>
2026-07-13 18:16:17 +0100

    Added in time param to routes
    plus extra guff to make it profesh

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/huntlyc/beacon-pi
 
-go 1.19
+go 1.26
 
 require github.com/duckfullstop/blinkybeacon v0.1.1
 
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 	"sync"
 	"time"
 
@@ -14,50 +15,106 @@ import (
 var beaconMu sync.Mutex
 
 func main() {
-	http.HandleFunc("/spin", spinReqHandler)
-	http.HandleFunc("/strobe", strobeReqHandler)
+	http.HandleFunc("GET /favicon.ico", faviconHandler)
+
+	// home
+	http.HandleFunc("GET /{$}", func(w http.ResponseWriter, r *http.Request){
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Hello, world!"))
+	})
+
+	// spin routes, time and default
+	http.HandleFunc("GET /spin", spinReqHandler)
+	http.HandleFunc("GET /spin/{time}/", spinReqHandler)
+
+	// stobe routes, time and default
+	http.HandleFunc("GET /strobe/{time}/", strobeReqHandler)
+	http.HandleFunc("GET /strobe", strobeReqHandler)
+
+
+	// catchall
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+		w.WriteHeader(http.StatusNotFound);
+		w.Write([]byte("Like my sanity, not found"))
+	})
 
 	log.Println("Server running on :1337")
 	log.Fatal(http.ListenAndServe(":1337", nil))
 }
 
+
+
+func getTimeInt(timeStr string) (int, error) {
+	var err error
+	time := 1
+
+	if timeStr != "" {
+		time, err = strconv.Atoi(timeStr)
+		if err == nil {
+			if time > 10 {
+				time = 10
+			} else if time < 1 {
+				time = 1
+			}
+		}
+	}
+
+	return time, err
+}
+
+func faviconHandler(w http.ResponseWriter, r *http.Request) {
+	// Tell the browser to expect an SVG image
+	w.Header().Set("Content-Type", "image/svg+xml")
+
+	// The SVG string with the alarm emoji
+	emojiSVG := `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><text y=".9em" font-size="90">🚨</text></svg>`
+
+	w.Write([]byte(emojiSVG))
+}
+
 func spinReqHandler(w http.ResponseWriter, r *http.Request) {
-	err := handleSpin()
+	timeStr := r.PathValue("time")
+	time, _ := getTimeInt(timeStr)
+
+	fmt.Printf("Spinnning for %v\n", time);
+	err := handleSpin(time)
 	if err != nil {
 		log.Println(err)
 		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("Err"))
+		_, _ = w.Write([]byte("Err"))
 		return
 	}
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("OK"))
+	_, _ = w.Write([]byte("OK"))
 }
 
 func strobeReqHandler(w http.ResponseWriter, r *http.Request) {
-	err := handleStrobeBeacon()
+	timeStr := r.PathValue("time")
+	time, _ := getTimeInt(timeStr)
+	fmt.Printf("stroe for %v\n", time);
+
+	err := handleStrobeBeacon(time)
 	if err != nil {
 		log.Println(err)
 		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("Err"))
+		_, _ = w.Write([]byte("Err"))
 		return
 	}
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("OK"))
+	_, _ = w.Write([]byte("OK"))
 }
 
-func handleSpin() error {
+func handleSpin(t int) error {
 	beaconMu.Lock()
 	defer beaconMu.Unlock()
 
-	runtime := 1 * time.Second
+	runtime := time.Duration(t) * time.Second
 
 	d, err := fsbeacon.OpenFarmBeacon()
 	if err != nil {
 		return err
 	}
 	defer d.Close()
-
-	fmt.Printf("Spinning beacon for %s.\n", runtime)
 
 	err = d.Spin()
 	if err != nil {
@@ -78,11 +135,11 @@ func handleSpin() error {
 	return nil
 }
 
-func handleStrobeBeacon() error {
+func handleStrobeBeacon(t int) error {
 	beaconMu.Lock()
 	defer beaconMu.Unlock()
 
-	runtime := 5 * time.Second
+	runtime := time.Duration(t) * time.Second
 
 	d, err := fsbeacon.OpenFarmBeacon()
 	if err != nil {
